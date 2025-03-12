30fcc1a6eec1f5993385318236556ce93867ced1
Yogi2084 <1si22is406@sit.ac.in>
2025-03-12 09:17:11 +0530

    Update randomNumber genarate , time , environment and puppit

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,66 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+const app = new Hono();
 
-const app = new Hono()
+const randomnumber : number[] = [];
+app.get("/genarate", (c) => {
+    const random=Math.floor(Math.random()*100);
+    randomnumber.push(random);  
+    return c.json({random}, 200); 
+       
+});
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+const time = new Date();
+app.get("/time", (c) => {
+    return c.json({time}, 200); 
+}
+);
+const environment:string[]= [];
+app.get("/environment", (c) => {   
+    const platform = process.platform; 
+    const environment=process.version
+    return c.json({environment,platform}, 200); 
+}
+);
+const puppit :string[] = [];
+app.get("/puppit", (c) => {
+    const name=c.req.query();
+    return c.json(`puppit:hello ${name}`, 200);
 
-serve({
-  fetch: app.fetch,
-  port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
+}); 
+ const randomNumber : number[] = [];
+app.post("/storenumber", async(c) => {
+    const body=await c.req.json();
+    const number = body.number;   
+    randomNumber.push(number);
+    return c.json({number}, 200); 
+}
+); 
+ app.post("/getnumber", async(c) => {
+    const body=await c.req.json();
+    const number = body.number;
+    return c.json({number}, 200);
+}
+);   
+
+const randomNum: number[] = [];
+
+app.post("/storenumber", async (c) => {
+  const body = await c.req.json();
+  const number = body.number;
+
+  if (typeof number !== "number") {
+    return c.json({ error: "Invalid number" }, 400);
+  }
+
+  randomNumber.push(number);
+  
+  return c.json({ lastEnteredNumber: number });
+});
+
+app.get("/storenumber", (c) => {
+  return c.json({ allNumbers: randomNumber });
+});
+
+serve(app);
+console.log(`Server is running at http://localhost:${3000}`);
